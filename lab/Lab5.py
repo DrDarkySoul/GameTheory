6e377cd7277aae1322d30dbcf9c92f2a0813a7a7
DrDarkySoul <valitov.rishat@gmail.com>
2019-05-31 09:24:21 +0300

    [WIP] Final

--- lab/Lab5.py
+++ lab/Lab5.py
@@ -1,84 +1,84 @@
-import numpy as np
-
-dim = 10
-epsilon = 10e-6
-control_low = 0
-control_high = 100
-
-
-def get_initial_opinions(control_l, control_h, dimension):
-    return np.random.randint(control_l, control_h, dimension)
+# -*- coding: utf-8 -*-
+import pydot
+import numpy.random as rand
+import networkx as nx
+from networkx.drawing.nx_pydot import write_dot
 
 
-def print_matrix(matrix):
-    print("Матрица доверия:")
-    print("\n".join(", ".join("{0:.3f}".format(x) for x in row) for row in matrix))
+players_count = 2
+tree_depth = 7
 
 
-def get_trust_matrix(dimension):
-    matrix = []
-    for _ in range(0, dimension):
-        row = np.random.sample(dimension)
-        matrix.append(row / row.sum())
-    print_matrix(matrix)
-    return np.array(matrix)
+def get_tree(depth, players):
+    tree = nx.balanced_tree(2, depth - 1)
+    for i in range(2 ** (depth - 1) - 1, 2 ** depth - 1):
+        wins = rand.randint(-10, 10, players)
+        tree.nodes[i]['wins'] = [tuple(wins)]
+    return tree
 
 
-def matrix_reduce(matrix, eps, opinions):
-    i = 0
-    reduced = True
-    while reduced:
-        i += 1
-        changed_opinions = matrix.dot(opinions).transpose()
-        if all(x <= eps for x in np.abs(opinions - changed_opinions)):
-            reduced = False
-        opinions = changed_opinions
-    return opinions, i
+def induction(tree, parent, players, player=0):
+    wins = []
+    max_win = 0
+    for subtree_root in tree[parent].keys():
+        if 'wins' not in tree[subtree_root] and subtree_root > parent:
+            induction(tree, subtree_root, players, (player + 1) % players)
+            node_max_win = tree.nodes[subtree_root]['wins'][0][player]
+            for win in tree.nodes[subtree_root]['wins']:
+                if win[player] > node_max_win:
+                    node_max_win = win[player]
+            if not len(wins) or node_max_win == max_win:
+                wins.extend(tree.nodes[subtree_root]['wins'])
+                max_win = node_max_win
+            elif node_max_win > max_win:
+                wins = tree.nodes[subtree_root]['wins']
+                max_win = node_max_win
+        if len(wins):
+            tree.nodes[parent]['wins'] = wins
 
 
-def solve(matrix, eps, op):
-    op_result, iter_count = matrix_reduce(matrix, eps, op)
-    print()
-    print("Начальные мнения агентов: X(0) =", op)
-    print("Количество итераций:", iter_count)
-    print("Результирующее мнение агентов без влияния: X(t->inf) =", ", ".join("{0:.3f}".format(x) for x in op_result))
-    print()
+def solve(tree, players):
+    ways_ = []
+    leafs = [name for name, node in tree.nodes.items() if 'wins' in node]
+    induction(tree, 0, players)
+    for leaf in leafs:
+        if tree.nodes[leaf]['wins'][0] in tree.nodes[0]['wins']:
+            ways_.append(nx.shortest_path(tree, 0, leaf))
+    return ways_
 
 
-def get_agents(dimension):
-    agents = np.arange(dimension)
-    np.random.shuffle(agents)
-    u_size, v_size = len(agents), len(agents)
-    while u_size + v_size > len(agents):
-        u_size = np.random.randint(1, len(agents))
-        v_size = np.random.randint(1, len(agents))
-    return agents[:u_size], agents[u_size:u_size + v_size]
+def get_labels(tree):
+    labels = {}
+    for k, v in tree.nodes.items():
+        wins_label = "\n" + "\n".join("/".join(str(x) for x in win) for win in v['wins']) if 'wins' in v else ""
+        labels[k] = str(k) + wins_label
+    return labels
 
 
-def get_controls(control_l, control_h, sign):
-    control = np.random.randint(control_l, control_h)
-    return control if sign else -control
+def print_tree(tree, file_name, ways_):
+    write_dot(tree, "tree.dot")
+    graph = pydot.graph_from_dot_file("tree.dot")[0]
+    graph.obj_dict['attributes']["size"] = '"10,10!"'
+    graph.obj_dict['attributes']["dpi"] = "720"
+
+    for k, v in get_labels(tree).items():
+        node = graph.get_node(str(k))[0]
+        node.set('label', v)
+        node.set('fontsize', '32')
+
+    colors_list = ['blue', 'green', 'red', 'yellow', 'brown', 'purple']
+    for way in ways_:
+        color = colors_list.pop(0)
+        for i in range(len(way) - 1):
+            edge = graph.get_edge(str(way[i]), str(way[i + 1]))[0]
+            if not edge.get('color'):
+                edge.set('color', color)
+            else:
+                graph.add_edge(pydot.Edge(way[i], way[i + 1], color=color))
+    graph.write_png(file_name)
 
 
-def solve_influence(matrix, dimension, eps):
-    u_agents, v_agents = get_agents(dimension)
-    print("Агенты первого игрока: {0}".format(sorted(u_agents)))
-    print("Агенты второго игрока: {0}".format(sorted(v_agents)))
-    u_control = get_controls(control_low, control_high, True)
-    v_control = get_controls(control_low, control_high, False)
-    print("Сформированное начальное мнение первого игрока: {0:.0f}".format(u_control))
-    print("Сформированное начальное мнение второго игрока: {0:.0f}".format(v_control))
-    influenced_opinions = initial_opinions
-    for number in np.hstack((v_agents, u_agents)):
-        influenced_opinions[number] = u_control if number in u_agents else v_control
-    print("Изначальные мнения с учетом сформированных: X(0) =", influenced_opinions)
-    result_opinions, iter_count = matrix_reduce(matrix, eps, influenced_opinions)
-    print("Количество итераций:", iter_count)
-    print("Результирующее мнение: X(t->inf) =", ", ".join("{0:.3f}".format(x) for x in result_opinions))
-
-
-initial_opinions = get_initial_opinions(control_low, control_high, dim)
-trust_matrix = get_trust_matrix(dim)
-
-solve(trust_matrix, epsilon, initial_opinions)
-solve_influence(trust_matrix, dim, epsilon)
+game = get_tree(tree_depth, players_count)
+print_tree(game, 'graph.png', [])
+ways = solve(game, players_count)
+print_tree(game, 'solved_graph.png', ways)
